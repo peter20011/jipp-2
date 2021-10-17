4fe743a2e5655d6d1b5916ddaaddc48d8da8bbb1
Piotr Warchołek <piotr.warcholek@student.pk.edu.pl>
2021-10-17 10:56:06 +0200

    Commit

--- simpleCalc/src/main.cpp
+++ simpleCalc/src/main.cpp
@@ -29,22 +29,33 @@ int main(int argc, char* argv[]) {
       help();
   }
 
-  if(argc==4){
-    if(strcmp(argv[1],"add")==0){
-        a=stoi(argv[2]);
-        b=stoi(argv[3]);
-        cout<<"Dodawanie: "<<add(a,b)<<endl;
-    }
-    else if(strcmp(argv[1],"subtract")==0){
-        a=stoi(argv[2]);
-        b=stoi(argv[3]);
-        cout<<"Odejmowanie: "<<subtract(a,b)<<endl;
+  if(argc==4) {
+      if (strcmp(argv[1], "add") == 0) {
+          a = stoi(argv[2]);
+          b = stoi(argv[3]);
+          cout << "Dodawanie: " << add(a, b) << endl;
+      } else if (strcmp(argv[1], "subtract") == 0) {
+          a = stoi(argv[2]);
+          b = stoi(argv[3]);
+          cout << "Odejmowanie: " << subtract(a, b) << endl;
+      } else {
+          err();
+          help();
+      }
+  }
+
+
+    if(argc==2){
+        if(strcmp(argv[1],"help")==0){
+            help();
+        }
+        else{
+            err();
+            help();
+        }
+
     }
     else{
         err();
-        help();
-    }
-
-  }
-
+        help();}
 }

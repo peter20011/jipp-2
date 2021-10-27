75a9fe490f22513bebcf90cbedfac87d4d70fc26
Piotr Warchołek <piotr.warcholek@student.pk.edu.pl>
2021-10-27 23:43:39 +0200

    commit

--- Project_matrix/matrixLib/src_matrix/matrixLib.cpp
+++ Project_matrix/matrixLib/src_matrix/matrixLib.cpp
@@ -18,11 +18,8 @@ using namespace  std;
  */
 int** addMatrix(int** a, int** b, int m, int n)
 {
-    int **C= new int *[m];
-    for(int i=0;i<m;i++)
-    {
-        C[i]=new int [n];
-    }
+    int **C;
+    C= allocation(m,n);
     for(int i=0;i<m;i++)
     {
         for(int j=0;j<n;j++)
